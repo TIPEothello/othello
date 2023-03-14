d901329b7d02a98e3b68e58e88e22eeba8ad1c18
rea1bacon <victor.lefevre2004@gmail.com>
2023-03-14 23:08:53 +0100

    fix des bugs de leon ......

--- src/main.rs
+++ src/main.rs
@@ -3,42 +3,44 @@
  Created Date: 14 Mar 2023
  Author: realbacon
  -----
- Last Modified: 14/03/2023 10:46:13
+ Last Modified: 14/03/2023 11:08:38
  Modified By: realbacon
  -----
  License  : MIT
  -----
 */
 
-
 mod board;
 mod rules;
-
-use board::Board;
-use board::Case;
+use board::{Board, Case};
+use crossterm::event::{read, Event, KeyCode, KeyEvent, KeyEventKind, KeyEventState, KeyModifiers};
 use rand::seq::SliceRandom;
-use crossterm::event::{read, Event, KeyCode, KeyEvent, KeyModifiers, KeyEventKind, KeyEventState};
+use rules::enemy;
 
 fn main() {
-	let mut rng = rand::thread_rng();
-	let mut board = Board::new();
-	let mut turn = Case::Black;
-	while board.available_moves(&turn).len() > 0 {
-		println!("{}", board);
-		// choose a random move within the available moves
-		let bmove = *board.available_moves(&turn).choose(&mut rng).unwrap();
-		board.make_move(bmove, turn).unwrap();
-		turn = enemy(&turn);
-		// Wait for user to press enter
+    let mut rng = rand::thread_rng();
+    let mut board = Board::new();
+    let mut turn = Case::Black;
+    while board.available_moves(&turn).len() > 0 {
+        println!("{}", board);
+        // choose a random move within the available moves
+        let bmove = *board.available_moves(&turn).choose(&mut rng).unwrap();
+        board.make_move(bmove, turn).unwrap();
+        turn = enemy(&turn);
+        // Wait for user to press enter
 
-		loop {
-			match read().unwrap() {
-				Event::Key(KeyEvent { code: KeyCode::Enter, modifiers: KeyModifiers::NONE, kind: KeyEventKind::Press, state: KeyEventState::NONE }) => break,
-				_ => (),
-			}
-		}
-
-	}
+        loop {
+            match read().unwrap() {
+                Event::Key(KeyEvent {
+                    code: KeyCode::Enter,
+                    modifiers: KeyModifiers::NONE,
+                    kind: KeyEventKind::Press,
+                    state: KeyEventState::NONE,
+                }) => break,
+                _ => (),
+            }
+        }
+    }
 
     println!("{}", board);
 
