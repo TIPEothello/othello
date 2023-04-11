1327735df66d0c899dcd5cf1780658db7f5bf05d
rea1bacon <40965674+rea1bacon@users.noreply.github.com>
2023-04-11 13:25:33 +0000

    nombre d'or

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
  Created Date: 21 Mar 2023
  Author: realbacon
  -----
- Last Modified: 4/04/2023 02:18:41
+ Last Modified: 11/04/2023 01:20:47
  Modified By: realbacon
  -----
  License  : MIT
@@ -17,12 +17,12 @@ mod rules;
 
 #[tokio::main(worker_threads = 10)]
 async fn main() {
-    /*let mut player = player::Player::new(Some((
+    let mut player = player::Player::new(Some((
         player::Strategy::Minimax { depth: 5 },
         player::Strategy::Random,
     )));
-    println!("{:?}", player.play_games(10).await);*/
+    println!("{:?}", player.play_games(100).await);
     let mut board = board::Board::new();
-    let tree = minimax::Tree::from_board(&mut board, None, 6);
-    println!("{:?}", tree);
+    //let tree = minimax::Tree::from_board(&mut board, None, 6);
+    //println!("{:?}", tree);
 }
