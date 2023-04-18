e03bee9be65a6339d7afd7026b8512d5a8c4e27c
Unaty498 <leon.muselli@gmail.com>
2023-04-18 19:44:12 +0200

    ça change un peu

--- src/main.rs
+++ src/main.rs
@@ -17,8 +17,11 @@ mod rules;
 
 #[tokio::main(worker_threads = 100)]
 async fn main() {
-    let board = board::Board::new();
-	let mut tree = minimax::Tree::from_board(&mut board.clone(), None, 6);
+    let mut board = board::Board::new();
+	for _ in 0..4 {
+		board.make_move(&board.available_moves(None)[0]).unwrap();
+	}
+	let mut tree = minimax::Tree::from_board(&mut board.clone(), None, 4);
 	minimax::minimax_tree(&mut tree, board::Case::Black);
 
 }
