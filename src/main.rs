2271174b37ba6a92aed7d4f6ceb140bcec8d5105
rea1bacon <victor.lefevre2004@gmail.com>
2023-04-23 17:44:10 +0200

    gradiant descent

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
  Created Date: 21 Mar 2023
  Author: realbacon
  -----
- Last Modified: 19/04/2023 05:17:33
+ Last Modified: 23/04/2023 05:43:58
  Modified By: realbacon
  -----
  License  : MIT
@@ -17,14 +17,14 @@ mod rules;
 
 #[tokio::main(worker_threads = 100)]
 async fn main() {
-    /*
-	let mut player = player::Player::new(Some((
+    gradiant_descent().await;
+    /*let mut player = player::Player::new(Some((
         player::Strategy::Manual,
-		player::Strategy::MinimaxTree { depth: 6 },
-	)));
-    
-	player.progressive_play()
-    */
+        player::Strategy::MinimaxTree { depth: 6 },
+    )));
+
+    player.progressive_play()
+
 
     let mut player = player::Player::new(Some((
         player::Strategy::Minimax { depth: 4 },
@@ -32,7 +32,7 @@ async fn main() {
     )));
     //player.progressive_play();
 
-    let result = player.play_games(100).await;
+    let result = player.play_games(500).await;
     println!(
         "Win ratio : White {}% ({}) - Black {}% ({})",
         result.0 as f32 / (result.0 + result.1 + result.2) as f32 * 100.0,
@@ -40,11 +40,31 @@ async fn main() {
         result.1 as f32 / (result.0 + result.1 + result.2) as f32 * 100.0,
         result.1
     );
-    /*
+
      let mut board = board::Board::new();
     for _ in 0..4 {
         board.make_move(&board.available_moves(None)[0]).unwrap();
     }
     let mut tree = minimax::Tree::from_board(&mut board.clone(), None, 4);
     minimax::minimax_tree(&mut tree, board::Case::Black);*/
+}
+use minimax::CoefS;
+async fn gradiant_descent() {
+    let step = 0.6;
+    let h = 0.0001;
+    let mut player = player::Player::new(Some((
+        player::Strategy::Minimax { depth: 4 },
+        player::Strategy::MinimaxTree { depth: 4 },
+    )));
+    for _ in 0..40 {
+        let mut coef = CoefS::from_file("coef.json");
+        let init_res = player.play_games(400).await;
+        // Modify the file coef.json
+        let ini = coef.material;
+        coef.material += h;
+        coef.modify_file("coef.json").unwrap();
+        let new_res = player.play_games(400).await;
+        let der = (new_res.1 - init_res.1) as f32 / h;
+        coef.material = ini - step * der;
+    }
 }
