c19bf2af6f4617eb8a63213147113e86c706e529
rea1bacon <victor.lefevre2004@gmail.com>
2023-05-10 22:53:04 +0200

    marche mais lent

--- src/scraper.rs
+++ src/scraper.rs
@@ -1,45 +1,63 @@
+/*
+ File: scraper.rs
+ Created Date: 10 May 2023
+ Author: realbacon
+ -----
+ Last Modified: 10/05/2023 10:51:47
+ Modified By: realbacon
+ -----
+ License  : MIT
+ -----
+*/
+
 use std::sync::Arc;
 
-use anyhow::{Result, Error};
 use crate::board::Case;
+use anyhow::{Error, Result};
+use headless_chrome::protocol::cdp::Page::CaptureScreenshotFormatOption::Png;
 use headless_chrome::{Browser, Tab};
-
 pub enum Difficulty {
     Easy = 0,
     Intermediate = 1,
-    Hard = 2
+    Hard = 2,
 }
 
 pub fn launch(color: Case, difficulty: Difficulty) -> Result<Arc<Tab>, Error> {
     let browser = Browser::default()?;
 
     let tab = browser.new_tab()?;
-    tab.navigate_to("http://reversi.fr/game.php")?;
-
+    tab.navigate_to("https://reversi.fr/game.php")?;
+    println!("Waiting for page to load...");
     match difficulty {
         Difficulty::Easy => {
             tab.wait_for_element("input#level0")?.click()?;
-        },
+        }
         Difficulty::Intermediate => {
             tab.wait_for_element("input#level1")?.click()?;
-        },
+        }
         Difficulty::Hard => {
             tab.wait_for_element("input#level2")?.click()?;
-        },
+        }
     }
 
     if color == Case::White {
         tab.wait_for_element("input#mode1")?.click()?;
     }
-
-    Ok(tab)
-}
-
-
-pub fn get_board(tab: Arc<Tab>) -> Result<[[Case; 8]; 8]> {
     let board = [[Case::Empty; 8]; 8];
 
     for element in tab.wait_for_elements("#game-content > table > tbody > tr:nth-child(1) > td:nth-child(1) > table > tbody > tr > td")? {
+        println!("{:?}", element.get_content()?);
+         
+    }
+    tab.bring_to_front()?;
+    Ok(tab)
+}
+
+pub fn get_board(tab: Arc<Tab>) -> Result<[[Case; 8]; 8]> {
+    let board = [[Case::Empty; 8]; 8];
+    println!("{:?}",tab.get_url());
+    let ele = tab.find_elements("#game-content > table > tbody > tr:nth-child(1) > td:nth-child(1) > table > tbody > tr > td")?;
+    for element in ele.iter() {
         println!("{:?}", element.get_attributes()?);
     }
 
