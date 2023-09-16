73ff025d5f8c826dfd4308ee4d4f09644a86720e
rea1bacon <victor.lefevre2004@gmail.com>
2023-09-16 21:06:23 +0200

    PlayerAPi struct -> PlayerApiTrait

--- src/player.rs
+++ src/player.rs
@@ -10,11 +10,12 @@
  -----
 */
 #![allow(dead_code, unused_imports)]
-
 use std::cmp::Ordering;
 use std::io::stdout;
+use std::process::{exit, ExitCode};
+use std::sync::Mutex;
 
-use crate::board::{Board, BoardState, Case, EndState};
+use crate::board::{self, Board, BoardState, Case, EndState};
 use crate::mcts;
 use crate::minimax::{self, Tree};
 use crossterm::cursor::MoveDown;
@@ -57,23 +58,20 @@ impl Player {
 
         let mut board = Board::new();
         let mut player1 =
-            PlayerAPI::new(self.strategy.0, PlayStyle::Progressive, Case::Black, &board);
+            new_player_api(self.strategy.0, PlayStyle::Progressive, Case::Black, &board);
         let mut player2 =
-            PlayerAPI::new(self.strategy.1, PlayStyle::Progressive, Case::White, &board);
+            new_player_api(self.strategy.1, PlayStyle::Progressive, Case::White, &board);
         println!();
         loop {
             println!("{}", board);
+            let mut quit = false;
             let (current_player, other) = match board.get_turn() {
                 Case::Black => (&mut player1, &mut player2),
                 Case::White => (&mut player2, &mut player1),
                 Case::Empty => panic!("wtf bro"),
             };
 
-            let (move_, mut quit) = current_player.get_move(&board);
-            if quit {
-                println!("Quitting...");
-                return;
-            }
+            let move_ = current_player.get_move(&board);
 
             let state = board.play_move(&move_).unwrap();
 
@@ -117,6 +115,26 @@ impl Player {
                     _ => {}
                 }
             }
+            loop {
+                match read().unwrap() {
+                    Event::Key(KeyEvent {
+                        code: KeyCode::Enter,
+                        modifiers: KeyModifiers::NONE,
+                        kind: KeyEventKind::Press,
+                        state: KeyEventState::NONE,
+                    }) => break,
+                    Event::Key(KeyEvent {
+                        code: KeyCode::Char('q'),
+                        modifiers: KeyModifiers::NONE,
+                        kind: KeyEventKind::Press,
+                        state: KeyEventState::NONE,
+                    }) => {
+                        quit = true;
+                        break;
+                    }
+                    _ => {}
+                }
+            }
             if quit {
                 println!("Quitting...");
                 return;
@@ -127,18 +145,18 @@ impl Player {
     pub async fn play_games(&mut self, n: u32) -> (u32, u32, u32) {
         use tokio::task::spawn;
         let mut games_result = (0, 0, 0); // Black White Draw
-
+        let vec: Mutex<(u32, u32, u32)> = Mutex::new((0, 0, 0));
         let games_result_par = (0..n)
             .into_par_iter()
             .map(|i| {
                 let mut board = Board::new();
-                let mut player1 = PlayerAPI::new(
+                let mut player1 = new_player_api(
                     self.strategy.0,
                     PlayStyle::Automatic,
                     Case::Black,
                     &board.clone(),
                 );
-                let mut player2 = PlayerAPI::new(
+                let mut player2 = new_player_api(
                     self.strategy.1,
                     PlayStyle::Automatic,
                     Case::White,
@@ -151,7 +169,7 @@ impl Player {
                         Case::Empty => panic!(""),
                     };
 
-                    let move_ = current_player.get_move(&board).0;
+                    let move_ = current_player.get_move(&board);
 
                     //println!("{:?}", move_);
 
@@ -162,6 +180,12 @@ impl Player {
                         other.update_board(&board);
                     } else if let BoardState::Ended(end_state) = state {
                         println!("finished game {i}");
+                        let mut locked = vec.lock().unwrap();
+                        match end_state {
+                            EndState::Winner(Case::Black) => locked.0 += 1,
+                            EndState::Winner(Case::White) => locked.1 += 1,
+                            EndState::Winner(Case::Empty) => locked.2 += 1,
+                        }
                         return end_state;
                     }
                 }
@@ -181,6 +205,7 @@ impl Player {
             .iter()
             .filter(|end_state| matches!(end_state, EndState::Winner(Case::Empty)))
             .count() as u32;
+        println!("{:?}", vec);
         games_result
     }
 }
@@ -193,92 +218,150 @@ pub struct PlayerAPI {
     rng: rand::rngs::ThreadRng,
 }
 
-impl PlayerAPI {
-    fn new(strategy: Strategy, playstyle: PlayStyle, player: Case, board: &Board) -> Self {
-        let (minimax, mcts) = match strategy {
-            Strategy::Greedy | Strategy::Manual | Strategy::Random => (
-                Tree::empty(),
-                mcts::MCTS::empty(),
-            ),
-            Strategy::MCTS { playout_budget } => (
-                Tree::empty(),
-                mcts::MCTS::new(player, playout_budget, board.clone()),
-            ),
-            Strategy::Minimax { depth } => (
-                Tree::from_board(&mut board.clone(), None, depth),
-                mcts::MCTS::empty(),
-            ),
-        };
+trait PlayerApiTrait {
+    fn update_board(&mut self, board: &Board);
+    fn get_move(&mut self, board: &Board) -> (usize, usize);
+}
 
-        Self {
-            mcts,
-            minimax,
-            strategy,
-            playstyle,
-            rng: rand::thread_rng(),
-        }
-    }
-
-    fn update_board(&mut self, board: &Board) {
-        if let Strategy::Minimax { depth } = self.strategy {
-            self.minimax = Tree::from_board(&mut board.clone(), None, depth);
-        };
-    }
-
-    fn get_move(&mut self, board: &Board) -> ((usize, usize), bool) {
-        match self.strategy {
-            Strategy::Random => (
-                *board.available_moves(None).choose(&mut self.rng).unwrap(),
-                false,
-            ),
-            Strategy::Greedy => (board.move_with_highest_gain().unwrap(), false),
-            Strategy::MCTS { .. } => (self.mcts.search(board), false),
-            Strategy::Minimax { .. } => (self.minimax.best_move(board.get_turn()), false),
-            Strategy::Manual => {
-                if matches!(self.playstyle, PlayStyle::Automatic) {
-                    panic!("Manual is not supported in automatic games !");
-                }
-                let moves = board.available_moves(None);
-                println!(
-                    "Available moves: {}",
-                    moves
-                        .iter()
-                        .map(print_coords)
-                        .collect::<Vec<String>>()
-                        .join(", ")
-                );
-                loop {
-                    // Get user input with std::io
-                    let mut input = String::new();
-                    std::io::stdin().read_line(&mut input).unwrap();
-                    let input = input.trim();
-                    let input = input
-                        .split("")
-                        .filter(|s| !s.is_empty())
-                        .collect::<Vec<&str>>();
-                    // Input can be A1 or a1
-                    if input.is_empty() {
-                        continue;
-                    }
-                    if input.len() != 2 {
-                        if input[0] == "q" {
-                            break ((0, 0), true);
-                        }
-                        println!("Invalid move");
-                        continue;
-                    }
-                    let input = (
-                        input[0].to_uppercase().chars().next().unwrap() as usize - 65,
-                        input[1].parse::<usize>().unwrap() - 1,
-                    );
-                    if !moves.contains(&input) {
-                        println!("Invalid move");
-                        continue;
-                    }
-                    break (input, false);
-                }
+fn new_player_api(
+    strategy: Strategy,
+    playstyle: PlayStyle,
+    player: Case,
+    board: &Board,
+) -> Box<dyn PlayerApiTrait> {
+    match strategy {
+        Strategy::Random => Box::new(RandomPlayerAPI),
+        Strategy::Greedy => Box::new(GreedyPlayerAPI),
+        Strategy::Manual => {
+            if matches!(playstyle, PlayStyle::Automatic) {
+                panic!("Manual is not supported in automatic games !");
             }
+            Box::new(ManualPlayerAPI)
         }
+        Strategy::Minimax { depth } => Box::new(MinimaxPlayerAPI::new(depth, board)),
+        Strategy::MCTS { playout_budget } => {
+            Box::new(MctsPlayerAPI::new(playout_budget, player, board))
+        }
+    }
+}
+
+struct MctsPlayerAPI(mcts::MCTS);
+
+impl MctsPlayerAPI {
+    #[inline]
+    fn new(playout_budget: usize, player: Case, board: &Board) -> Self {
+        Self(mcts::MCTS::new(player, playout_budget, board.clone()))
+    }
+}
+
+impl PlayerApiTrait for MctsPlayerAPI {
+    #[inline]
+    fn update_board(&mut self, _board: &Board) {
+        ()
+    }
+    #[inline]
+    fn get_move(&mut self, board: &Board) -> (usize, usize) {
+        self.0.search(board)
+    }
+}
+
+struct ManualPlayerAPI;
+
+impl PlayerApiTrait for ManualPlayerAPI {
+    #[inline]
+    fn update_board(&mut self, _board: &Board) {
+        ()
+    }
+
+    fn get_move(&mut self, board: &Board) -> (usize, usize) {
+        let moves = board.available_moves(None);
+        println!(
+            "Available moves: {}",
+            moves
+                .iter()
+                .map(print_coords)
+                .collect::<Vec<String>>()
+                .join(", ")
+        );
+        loop {
+            // Get user input with std::io
+            let mut input = String::new();
+            std::io::stdin().read_line(&mut input).unwrap();
+            let input = input.trim();
+            let input = input
+                .split("")
+                .filter(|s| !s.is_empty())
+                .collect::<Vec<&str>>();
+            // Input can be A1 or a1
+            if input.is_empty() {
+                continue;
+            }
+            if input.len() != 2 {
+                if input[0] == "q" {
+                    exit(130);
+                }
+                println!("Invalid move");
+                continue;
+            }
+            let input = (
+                input[0].to_uppercase().chars().next().unwrap() as usize - 65,
+                input[1].parse::<usize>().unwrap() - 1,
+            );
+            if !moves.contains(&input) {
+                println!("Invalid move");
+                continue;
+            }
+            break (input);
+        }
+    }
+}
+
+struct MinimaxPlayerAPI(Tree);
+
+impl MinimaxPlayerAPI {
+    #[inline]
+    fn new(depth: u8, board: &Board) -> Self {
+        Self(Tree::from_board(&mut board.clone(), None, depth))
+    }
+}
+
+impl PlayerApiTrait for MinimaxPlayerAPI {
+    #[inline]
+    fn update_board(&mut self, board: &Board) {
+        self.0 = Tree::from_board(&mut board.clone(), None, self.0.depth);
+    }
+    #[inline]
+    fn get_move(&mut self, board: &Board) -> (usize, usize) {
+        self.0.best_move(board.get_turn())
+    }
+}
+
+struct RandomPlayerAPI;
+
+impl PlayerApiTrait for RandomPlayerAPI {
+    #[inline]
+    fn update_board(&mut self, _board: &Board) {
+        ()
+    }
+    #[inline]
+    fn get_move(&mut self, board: &Board) -> (usize, usize) {
+        *board
+            .available_moves(None)
+            .choose(&mut rand::thread_rng())
+            .unwrap()
+    }
+}
+
+struct GreedyPlayerAPI;
+
+impl PlayerApiTrait for GreedyPlayerAPI {
+    #[inline]
+    fn update_board(&mut self, _board: &Board) {
+        ()
+    }
+    #[inline]
+    fn get_move(&mut self, board: &Board) -> (usize, usize) {
+        board.move_with_highest_gain().unwrap()
     }
 }
 
