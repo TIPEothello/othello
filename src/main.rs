a917b63fb0ee6775ac8f34e789856b466166507d
Unaty498 <50841236+Unaty498@users.noreply.github.com>
2024-05-31 09:33:34 +0000

    add length option for versbose

--- src/main.rs
+++ src/main.rs
@@ -9,16 +9,15 @@ mod rules;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let budget1 = &args[1].parse::<usize>().unwrap();
-    let budget2 = &args[2].parse::<usize>().unwrap();
+    let budget2 = &args[2].parse::<u8>().unwrap();
     let mut player = player::Player::new((
         player::Strategy::MCTS {
             playout_budget: *budget1,
             final_solve: true,
         },
-        player::Strategy::MCTS {
-            playout_budget: *budget2,
-            final_solve: false,
-        },
+        player::Strategy::Minimax { depth: *budget2 }
+
+,
     ));
-    player.play_games(100, true);
+    player.play_games(100, true, 70);
 }
